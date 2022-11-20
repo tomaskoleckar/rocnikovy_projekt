ee9dca83af7c08376d0543ca9428bd26fa0a7d9b
Tomáš Kolečkář <tomanak159@seznam.cz>
2022-11-20 16:51:26 +0100

    databaseClass

--- app/src/main/java/com/example/wgg_v01/MainActivity.kt
+++ app/src/main/java/com/example/wgg_v01/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.wgg_v01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var username = findViewById(R.id.username) as EditText
+        var password = findViewById(R.id.password) as EditText
+        var signin = findViewById(R.id.signin) as Button
+        var signup = findViewById(R.id.signup) as Button
+
+        signup.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+
+
+            }
+
+        })
+
+        signin.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+
+
+            }
+
+        })
+
+
+
     }
 }
