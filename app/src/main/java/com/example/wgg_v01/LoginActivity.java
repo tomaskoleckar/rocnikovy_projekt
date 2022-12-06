44004891361cd3c805130133b2220dfd34f97fe6
Tomáš Kolečkář <tomanak159@seznam.cz>
2022-12-06 18:58:28 +0100

    loginRegisterV1

--- app/src/main/java/com/example/wgg_v01/LoginActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.wgg_v01;
-
-import android.app.Activity;
-
-public class LoginActivity extends Activity {
-}
--- app/src/main/java/com/example/wgg_v01/MainActivity.kt
+++ app/src/main/java/com/example/wgg_v01/MainActivity.kt
@@ -7,7 +7,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.wgg_v01.ui.login.HomeActivity
 import com.example.wgg_v01.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
